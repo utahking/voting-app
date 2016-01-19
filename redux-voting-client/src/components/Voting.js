41da8fc60e776275319d1642e78a32f1437e80e0
Jon King <jonking@gmail.com>
2016-01-19 08:20:13 -0800

    Initial Commit

---
+++ redux-voting-client/src/components/Voting.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
+
+class Voting extends React.Component {
+	render(){
+		return (
+			<div>{this.props.winner ?
+        		<Winner ref="winner" winner={this.props.winner} /> :
+        		<Vote {...this.props} />
+        		}
+			</div>
+		);
+	}
+}
+
+export default Voting;
